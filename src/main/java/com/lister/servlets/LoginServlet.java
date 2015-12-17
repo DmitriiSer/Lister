8a2d77ee5fd650d04e6ee60d9db7e151f2c6bc71
Dmitrii <dmitrii.serikov@gmail.com>
2015-12-17 16:15:11 -0500

    Made it work on Android emulator

--- src/main/java/com/lister/servlets/LoginServlet.java
+++ src/main/java/com/lister/servlets/LoginServlet.java
@@ -101,6 +101,8 @@ public class LoginServlet extends HttpServlet {
                     userProfile.setListTitles(lists);
                     // get session object and set session relative attributes
                     HttpSession session = request.getSession(false);
+                    if (session == null)
+                        session = request.getSession(true);
                     logger.info("session = " + session);
                     session.setAttribute("RemoteIP", new String(request.getRemoteAddr()));
                     logger.info("RemoteIP = " + new String(request.getRemoteAddr()));
